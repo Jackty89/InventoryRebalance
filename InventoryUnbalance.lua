9c8f1cae3b758edf1a3006b5565614a2491e029e
Jackty89 <90223197+Jackty89@users.noreply.github.com>
2022-03-11 22:14:24 +0100

    formatting

--- InventoryUnbalance.lua
+++ InventoryUnbalance.lua
@@ -12,29 +12,29 @@ StackMultiPlierFreighterAndCargoNormal = "2" -- 100 000 and 5000 after multiplie
 RefundNormal = "1"  -- Refund from base Deconstruct (Original 0.5). This will give you a 100% refund
 
 --ExoSuit Tech Slot 8 x 6 = 48
-TechWidthNormal = "8" -- Tech slots increase 
+TechWidthNormal = "8" -- Tech slots increase
 TechHeightNormal = "6" -- Tech slots increase
 
 SubstanceAndProcductStackSizeLimit = "9999999" -- (Original 9999)
 
-NMS_MOD_DEFINITION_CONTAINER = 
+NMS_MOD_DEFINITION_CONTAINER =
 {
   ["MOD_FILENAME"] 			= ModName..GameVersion..".pak",
-  ["MOD_DESCRIPTION"]		= "Unbalance of InvetorySize and Deconstruction cost",         
-  ["MOD_AUTHOR"]			= "Jackty89",         
-  ["NMS_VERSION"]			= GameVersion,     
-  ["MODIFICATIONS"] 		= 
+  ["MOD_DESCRIPTION"]		= "Unbalance of InvetorySize and Deconstruction cost",
+  ["MOD_AUTHOR"]			= "Jackty89",
+  ["NMS_VERSION"]			= GameVersion,
+  ["MODIFICATIONS"] 		=
     {
 	    {
-			["MBIN_CHANGE_TABLE"] 	= 
-			{ 
+			["MBIN_CHANGE_TABLE"] 	=
+			{
 				{
 					["MBIN_FILE_SOURCE"] 	= FilePath,
-					["EXML_CHANGE_TABLE"] 	= 
+					["EXML_CHANGE_TABLE"] 	=
 					{
 						{
 							["PRECEDING_KEY_WORDS"] = "",
-							["VALUE_CHANGE_TABLE"] 	= 
+							["VALUE_CHANGE_TABLE"] 	=
 							{
 								{"DefaultSubstanceMaxAmount",			        SubstanceDefaultStackSizeNormal},
 								{"DefaultProductMaxAmount",                     ProductDefaultStackSizeNormal},
@@ -59,7 +59,7 @@ NMS_MOD_DEFINITION_CONTAINER =
 								{"SubstanceMaxAmountLimit",                     SubstanceAndProcductStackSizeLimit},
 								{"ProductMaxAmountLimit",                       SubstanceAndProcductStackSizeLimit},
 								{"PlayerPersonalInventoryTechWidth",            TechWidthNormal},
-                                {"PlayerPersonalInventoryTechHeight",           TechHeightNormal},	
+                                {"PlayerPersonalInventoryTechHeight",           TechHeightNormal},
 								{"DeconstructRefundPercentage",                 RefundNormal}
 							}
 						}
